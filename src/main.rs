a99bfb7a06d3a54b367db47b69f00d195600f068
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-26 11:17:47 +0200

    Finalize getting stats

--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,20 @@ fn main() -> Result<(), std::io::Error> {
     BufReader::new(file).read_to_string(&mut buf)?;
 
     let mut stats_map: HashMap<String, MonthlyStat> = HashMap::new();
-    let mut stats = buf
-        .split("==============")
+    buf.split("==============")
         .map(MonthlyStat::parse)
-        .collect::<Vec<MonthlyStat>>();
+        .collect::<Vec<MonthlyStat>>()
+        .into_iter()
+        .for_each(|ns| {
+            match stats_map.get_mut(&ns.month) {
+                Some(s) => *s += ns,
+                None => {
+                    stats_map.insert(ns.month.clone(), ns);
+                }
+            };
+        });
 
-    stats.iter_mut().for_each(|ns| {
-        match stats_map.get_mut(&ns.month) {
-            Some(s) => (),
-            None => (),
-        };
-    });
+    println!("{:?}", stats_map);
 
     Ok(())
 }
