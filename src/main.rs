942a12f15cbad574d0c1b1d8409f6bd926999963
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-26 13:04:22 +0200

    Add printing of labels

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{
     io::{BufReader, Read},
 };
 
-use charts::{Chart, LineSeriesView, MarkerType, PointLabelPosition, ScaleLinear};
+use charts::{AxisPosition, Chart, Color, LineSeriesView, MarkerType, ScaleLinear};
 use indexmap::IndexMap;
 use monthly_stat::MonthlyStat;
 
@@ -37,15 +37,14 @@ fn draw_chart(stats: &[MonthlyStat]) {
     let first_month = &stats.first().unwrap().month;
     let last_month = &stats.last().unwrap().month;
 
-    // Define chart related sizes.
-    let width = 800;
-    let height = 600;
-    let (top, right, bottom, left) = (90, 40, 50, 60);
+    let width = 1920;
+    let height = 800;
+    let (top, right, bottom, left) = (50, 200, 50, 100);
 
     // Create a band scale that will interpolate values in [0, 200] to values in the
     // [0, availableWidth] range (the width of the chart without the margins).
     let x = ScaleLinear::new()
-        .set_domain(vec![0_f32, 200_f32])
+        .set_domain(vec![0_f32, stats.len() as f32])
         .set_range(vec![0, width - left - right]);
 
     // Create a linear scale that will interpolate values in [0, 100] range to corresponding
@@ -54,32 +53,70 @@ fn draw_chart(stats: &[MonthlyStat]) {
     // in top left corner, while chart's origin is in bottom left corner, hence we need to invert
     // the range on Y axis for the chart to display as though its origin is at bottom left.
     let y = ScaleLinear::new()
-        .set_domain(vec![0_f32, 100_f32])
+        .set_domain(vec![0_f32, 400000_f32])
         .set_range(vec![height - top - bottom, 0]);
 
     // You can use your own iterable as data as long as its items implement the `PointDatum` trait.
-    let line_data = vec![(12, 54), (100, 40), (120, 50), (180, 70)];
+    let mut users = vec![];
+    let mut lessons = vec![];
+    let mut project_submissions = vec![];
+    let mut projects_liked = vec![];
 
-    // Create Line series view that is going to represent the data.
-    let line_view = LineSeriesView::new()
+    stats.iter().enumerate().for_each(|(i, s)| {
+        users.push((i as f32, s.users as f32));
+        lessons.push((i as f32, s.lessons as f32));
+        project_submissions.push((i as f32, s.projects_liked as f32));
+        projects_liked.push((i as f32, s.project_submissions as f32));
+    });
+
+    let users_view = LineSeriesView::new()
         .set_x_scale(&x)
         .set_y_scale(&y)
-        .set_marker_type(MarkerType::Circle)
-        .set_label_position(PointLabelPosition::N)
-        .load_data(&line_data)
+        .set_custom_data_label("User Sign Ups".to_string())
+        .set_label_visibility(false)
+        .load_data(&users)
         .unwrap();
 
-    // Generate and save the chart.
+    let lessons_view = LineSeriesView::new()
+        .set_x_scale(&x)
+        .set_y_scale(&y)
+        .set_custom_data_label("Lessons Completed".to_string())
+        .set_colors(Color::from_vec_of_hex_strings(vec!["#aaaa00"]))
+        .set_label_visibility(false)
+        .load_data(&lessons)
+        .unwrap();
+
+    let project_submissions_view = LineSeriesView::new()
+        .set_x_scale(&x)
+        .set_y_scale(&y)
+        .set_label_visibility(false)
+        .set_custom_data_label("Project Submissions".to_string())
+        .set_colors(Color::from_vec_of_hex_strings(vec!["#aa0000"]))
+        .load_data(&project_submissions)
+        .unwrap();
+
+    let projects_liked_view = LineSeriesView::new()
+        .set_x_scale(&x)
+        .set_y_scale(&y)
+        .set_label_visibility(false)
+        .set_custom_data_label("Projects Liked".to_string())
+        .set_colors(Color::from_vec_of_hex_strings(vec!["#555555"]))
+        .load_data(&projects_liked)
+        .unwrap();
+
     Chart::new()
         .set_width(width)
         .set_height(height)
         .set_margins(top, right, bottom, left)
-        .add_title(String::from("Line Chart"))
-        .add_view(&line_view)
+        .add_title(String::from("TOP Growth"))
+        .add_view(&users_view)
+        .add_view(&lessons_view)
+        .add_view(&project_submissions_view)
+        .add_view(&projects_liked_view)
         .add_axis_bottom(&x)
         .add_axis_left(&y)
-        .add_left_axis_label("Custom Y Axis Label")
-        .add_bottom_axis_label("Custom X Axis Label")
+        .add_bottom_axis_label(format!("{} - {}", first_month, last_month))
+        .add_legend_at(AxisPosition::Right)
         .save("line-chart.svg")
         .unwrap();
 }
