b61113fdf9804d3052b151bc3202d8400b7ccab9
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-26 10:00:10 +0200

    Add new test

--- src/monthly_stat.rs
+++ src/monthly_stat.rs
@@ -42,4 +42,24 @@ mod test {
         let result = MonthlyStat::parse(str);
         assert_eq!(expected, result);
     }
+
+    #[test]
+    fn parse_another_stat() {
+        let expected = MonthlyStat {
+            month: "June 1999".to_string(),
+            users: 2020,
+            lessons: 1010,
+            project_submissions: 2,
+            projects_liked: 300,
+        };
+
+        let str = r#"June 22th, 1999
+2020 users signed up
+1010 lessons completed
+2 project submissions added
+300 projects liked"#;
+
+        let result = MonthlyStat::parse(str);
+        assert_eq!(expected, result);
+    }
 }
