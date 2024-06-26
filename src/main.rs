2e34c19a95a7a6c6b7c02c19b6c443c8de512de9
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-06-26 12:02:38 +0200

    Add building charts and use indexmap for insertion order preserve

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::{
-    collections::HashMap,
     fs::File,
     io::{BufReader, Read},
 };
 
+use charts::{Chart, LineSeriesView, MarkerType, PointLabelPosition, ScaleLinear};
+use indexmap::IndexMap;
 use monthly_stat::MonthlyStat;
 
 mod monthly_stat;
@@ -13,14 +14,11 @@ fn main() -> Result<(), std::io::Error> {
     let mut buf = String::new();
     BufReader::new(file).read_to_string(&mut buf)?;
 
-    let mut stats_map: HashMap<String, MonthlyStat> = HashMap::new();
+    let mut stats_map: IndexMap<String, MonthlyStat> = IndexMap::new();
     let stats = buf
         .split("==============")
         .map(MonthlyStat::parse)
         .collect::<Vec<MonthlyStat>>();
-
-    let first_month = &stats.first().unwrap().month;
-    let last_month = &stats.last().unwrap().month;
 
     stats.into_iter().for_each(|ns| {
         match stats_map.get_mut(&ns.month) {
@@ -31,5 +29,57 @@ fn main() -> Result<(), std::io::Error> {
         };
     });
 
+    draw_chart(&stats_map.into_values().collect::<Vec<MonthlyStat>>());
     Ok(())
+}
+
+fn draw_chart(stats: &[MonthlyStat]) {
+    let first_month = &stats.first().unwrap().month;
+    let last_month = &stats.last().unwrap().month;
+
+    // Define chart related sizes.
+    let width = 800;
+    let height = 600;
+    let (top, right, bottom, left) = (90, 40, 50, 60);
+
+    // Create a band scale that will interpolate values in [0, 200] to values in the
+    // [0, availableWidth] range (the width of the chart without the margins).
+    let x = ScaleLinear::new()
+        .set_domain(vec![0_f32, 200_f32])
+        .set_range(vec![0, width - left - right]);
+
+    // Create a linear scale that will interpolate values in [0, 100] range to corresponding
+    // values in [availableHeight, 0] range (the height of the chart without the margins).
+    // The [availableHeight, 0] range is inverted because SVGs coordinate system's origin is
+    // in top left corner, while chart's origin is in bottom left corner, hence we need to invert
+    // the range on Y axis for the chart to display as though its origin is at bottom left.
+    let y = ScaleLinear::new()
+        .set_domain(vec![0_f32, 100_f32])
+        .set_range(vec![height - top - bottom, 0]);
+
+    // You can use your own iterable as data as long as its items implement the `PointDatum` trait.
+    let line_data = vec![(12, 54), (100, 40), (120, 50), (180, 70)];
+
+    // Create Line series view that is going to represent the data.
+    let line_view = LineSeriesView::new()
+        .set_x_scale(&x)
+        .set_y_scale(&y)
+        .set_marker_type(MarkerType::Circle)
+        .set_label_position(PointLabelPosition::N)
+        .load_data(&line_data)
+        .unwrap();
+
+    // Generate and save the chart.
+    Chart::new()
+        .set_width(width)
+        .set_height(height)
+        .set_margins(top, right, bottom, left)
+        .add_title(String::from("Line Chart"))
+        .add_view(&line_view)
+        .add_axis_bottom(&x)
+        .add_axis_left(&y)
+        .add_left_axis_label("Custom Y Axis Label")
+        .add_bottom_axis_label("Custom X Axis Label")
+        .save("line-chart.svg")
+        .unwrap();
 }
